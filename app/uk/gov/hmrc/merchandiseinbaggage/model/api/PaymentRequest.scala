5fdb8516cadf4d0262874c2c91e9f8cc9eef45bd
Pasquale Gatto <47112536+PasGat@users.noreply.github.com>
2020-08-27 14:45:52 +0100

    MIBM-37: paymentRequest to declaration refactoring

--- app/uk/gov/hmrc/merchandiseinbaggage/model/api/PaymentRequest.scala
+++ app/uk/gov/hmrc/merchandiseinbaggage/model/api/PaymentRequest.scala
@@ -5,10 +5,20 @@
 
 package uk.gov.hmrc.merchandiseinbaggage.model.api
 
+import java.util.UUID
+
 import play.api.libs.json.{Format, Json}
-import uk.gov.hmrc.merchandiseinbaggage.model.core.{Amount, ChargeReference, CsgTpsProviderId, TraderName}
+import uk.gov.hmrc.merchandiseinbaggage.model.core.{Amount, ChargeReference, CsgTpsProviderId, Declaration, DeclarationId, Outstanding, TraderName}
 
 case class PaymentRequest(traderName: TraderName, amount: Amount, csgTpsProviderId: CsgTpsProviderId, chargeReference: ChargeReference)
 object PaymentRequest {
   implicit val format: Format[PaymentRequest] = Json.format
+
+  implicit class ToDeclaration(paymentRequest: PaymentRequest) {
+    def toDeclarationInInitialState: Declaration = {
+      import paymentRequest._
+      Declaration(DeclarationId(UUID.randomUUID().toString), traderName, amount, csgTpsProviderId,
+        chargeReference, Outstanding, None, None)
+    }
+  }
 }
--- test/uk/gov/hmrc/merchandiseinbaggage/model/api/PaymentRequestSpec.scala
+++ test/uk/gov/hmrc/merchandiseinbaggage/model/api/PaymentRequestSpec.scala
@@ -6,6 +6,7 @@
 package uk.gov.hmrc.merchandiseinbaggage.model.api
 
 import play.api.libs.json.Json
+import uk.gov.hmrc.merchandiseinbaggage.model.core.{Declaration, Outstanding}
 import uk.gov.hmrc.merchandiseinbaggage.{BaseSpecWithMongoTestServer, CoreTestData}
 
 class PaymentRequestSpec extends BaseSpecWithMongoTestServer with CoreTestData {
@@ -17,4 +18,12 @@ class PaymentRequestSpec extends BaseSpecWithMongoTestServer with CoreTestData {
     Json.toJson(paymentRequest) mustBe Json.parse(actual)
   }
 
+  "convert a payment request in to an outstanding declaration with no recorded payments and reconciliation" in {
+    val actualDeclaration: Declaration = aPaymentRequest.toDeclarationInInitialState
+
+    actualDeclaration must matchPattern { case Declaration(_, _, _, _, _, _, _, _) => }
+    actualDeclaration.paymentStatus mustBe Outstanding
+    actualDeclaration.paid mustBe empty
+    actualDeclaration.reconciled mustBe empty
+  }
 }
