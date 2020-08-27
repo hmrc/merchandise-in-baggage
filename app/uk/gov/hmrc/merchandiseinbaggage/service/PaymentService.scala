c8dd4c860945e24fa6cb1073449fd4bd7210feac
Pasquale Gatto <47112536+PasGat@users.noreply.github.com>
2020-08-27 15:43:31 +0100

    MIBM-37: paymentService to convert request to declaration

--- app/uk/gov/hmrc/merchandiseinbaggage/service/PaymentService.scala
+++ app/uk/gov/hmrc/merchandiseinbaggage/service/PaymentService.scala
@@ -5,12 +5,17 @@
 
 package uk.gov.hmrc.merchandiseinbaggage.service
 
+import uk.gov.hmrc.merchandiseinbaggage.model.api.PaymentRequest
 import uk.gov.hmrc.merchandiseinbaggage.model.core.Declaration
 
 import scala.concurrent.{ExecutionContext, Future}
+import scala.util.Try
 
 trait PaymentService {
 
-  def persistDeclaration(persist: Declaration => Future[Boolean], declaration: Declaration)(implicit ec: ExecutionContext) =
-    persist(declaration).map(_ => declaration)
+  def persistDeclaration(persist: Declaration => Future[Boolean], paymentRequest: PaymentRequest)(implicit ec: ExecutionContext): Future[Declaration] =
+    for {
+      declaration <- Future.fromTry(Try(paymentRequest.toDeclarationInInitialState))
+      _           <- persist(declaration)
+    } yield declaration
 }
--- test/uk/gov/hmrc/merchandiseinbaggage/service/PaymentServiceSpec.scala
+++ test/uk/gov/hmrc/merchandiseinbaggage/service/PaymentServiceSpec.scala
@@ -6,6 +6,7 @@
 package uk.gov.hmrc.merchandiseinbaggage.service
 
 import org.scalatest.concurrent.ScalaFutures
+import uk.gov.hmrc.merchandiseinbaggage.model.api.PaymentRequest
 import uk.gov.hmrc.merchandiseinbaggage.model.core.Declaration
 import uk.gov.hmrc.merchandiseinbaggage.{BaseSpecWithApplication, CoreTestData}
 
@@ -14,12 +15,14 @@ import scala.concurrent.Future
 
 class PaymentServiceSpec extends BaseSpecWithApplication with CoreTestData with ScalaFutures {
 
-  "persist a declaration" in new PaymentService {
+  "persist a declaration from a payment request" in new PaymentService {
     val persist: Declaration => Future[Boolean] = _ => Future.successful(true)
-    val declaration: Declaration = aDeclaration
+    val paymentRequest: PaymentRequest = aPaymentRequest
+    import paymentRequest._
 
-    whenReady(persistDeclaration(persist, declaration)) { result =>
-      result mustBe declaration
+    whenReady(persistDeclaration(persist, paymentRequest)) { result =>
+      result mustBe Declaration(result.declarationId, traderName, amount, csgTpsProviderId,
+        chargeReference, result.paymentStatus, result.paid, result.reconciled)
     }
   }
 }
