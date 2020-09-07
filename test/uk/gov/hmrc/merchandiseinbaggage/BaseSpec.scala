6e91d29838ed49dd45982361e98367c445cdb7c5
Pasquale Gatto <47112536+PasGat@users.noreply.github.com>
2020-09-07 07:34:56 +0100

    MIBM-37: this should fix build

--- test/uk/gov/hmrc/merchandiseinbaggage/BaseSpec.scala
+++ test/uk/gov/hmrc/merchandiseinbaggage/BaseSpec.scala
@@ -5,28 +5,21 @@
 
 package uk.gov.hmrc.merchandiseinbaggage
 
-import de.bwaldvogel.mongo.MongoServer
-import de.bwaldvogel.mongo.backend.memory.MemoryBackend
 import org.scalatest.matchers.must.Matchers
 import org.scalatest.wordspec.AnyWordSpec
 import org.scalatest.{BeforeAndAfterAll, BeforeAndAfterEach}
 import org.scalatestplus.play.guice.GuiceOneAppPerSuite
 import play.api.inject.Injector
 import play.api.mvc.AnyContentAsEmpty
+import play.api.test.CSRFTokenHelper._
 import play.api.test.FakeRequest
 import play.api.test.Helpers._
-import play.api.test.CSRFTokenHelper._
 import uk.gov.hmrc.merchandiseinbaggage.config.{AppConfig, MongoConfiguration}
 
 
 trait BaseSpec extends AnyWordSpec with Matchers with BeforeAndAfterEach with BeforeAndAfterAll
 
 trait BaseSpecWithApplication extends BaseSpec with GuiceOneAppPerSuite with MongoConfiguration {
-  private val server: MongoServer = new MongoServer(new MemoryBackend())
-  server.bind(mongoConf.host, mongoConf.port)
-
-  override def afterAll(): Unit = server.shutdown()
-
   def injector: Injector = app.injector
   implicit val appConf: AppConfig = new AppConfig
 
