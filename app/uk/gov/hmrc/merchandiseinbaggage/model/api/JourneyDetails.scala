dd82a05bcb673534188699c82cdfb6289430c43d
Pasquale Gatto <47112536+PasGat@users.noreply.github.com>
2021-01-19 15:51:59 +0000

    MIB-423: extract into single files

---
+++ app/uk/gov/hmrc/merchandiseinbaggage/model/api/JourneyDetails.scala
@@ -0,0 +1,66 @@
+/*
+ * Copyright 2021 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.merchandiseinbaggage.model.api
+
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
+
+import play.api.libs.json.{JsObject, JsResult, JsSuccess, JsValue, Json, OFormat}
+import uk.gov.hmrc.merchandiseinbaggage.model.api.YesNo.{No, Yes}
+
+sealed trait JourneyDetails {
+  val port: Port
+  val dateOfTravel: LocalDate
+  val formattedDateOfArrival: String = DateTimeFormatter.ofPattern("dd MMM yyyy").format(dateOfTravel)
+  val travellingByVehicle: YesNo = No
+  val maybeRegistrationNumber: Option[String] = None
+}
+
+object JourneyDetails {
+  implicit val format: OFormat[JourneyDetails] = new OFormat[JourneyDetails] {
+    override def reads(json: JsValue): JsResult[JourneyDetails] = {
+      val port = (json \ "port").as[Port]
+      val dateOfTravel = (json \ "dateOfTravel").as[LocalDate]
+
+      (json \ "registrationNumber").asOpt[String] match {
+        case Some(regNo) => JsSuccess(JourneyInSmallVehicle(port, dateOfTravel, regNo))
+        case None        => JsSuccess(JourneyOnFoot(port, dateOfTravel))
+      }
+    }
+
+    override def writes(o: JourneyDetails): JsObject =
+      o match {
+        case p: JourneyOnFoot         => JourneyOnFoot.format.writes(p)
+        case p: JourneyInSmallVehicle => JourneyInSmallVehicle.format.writes(p)
+      }
+  }
+}
+
+case class JourneyOnFoot(port: Port, dateOfTravel: LocalDate) extends JourneyDetails
+
+object JourneyOnFoot {
+  implicit val format: OFormat[JourneyOnFoot] = Json.format[JourneyOnFoot]
+}
+
+case class JourneyInSmallVehicle(port: Port, dateOfTravel: LocalDate, registrationNumber: String) extends JourneyDetails {
+  override val travellingByVehicle: YesNo = Yes
+  override val maybeRegistrationNumber: Option[String] = Some(registrationNumber)
+}
+
+object JourneyInSmallVehicle {
+  implicit val format: OFormat[JourneyInSmallVehicle] = Json.format[JourneyInSmallVehicle]
+}
