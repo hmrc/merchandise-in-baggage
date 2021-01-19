03d7fd5e5cc221e25e5a2dd495bf83fb0a939066
Pasquale Gatto <47112536+PasGat@users.noreply.github.com>
2021-01-19 16:02:04 +0000

    MIB-423: extract into single files (#55)

---
+++ app/uk/gov/hmrc/merchandiseinbaggage/model/api/AmountInPence.scala
@@ -0,0 +1,31 @@
+/*
+ * Copyright 2021 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.merchandiseinbaggage.model.api
+
+import java.text.NumberFormat.getCurrencyInstance
+import java.util.Locale.UK
+import play.api.libs.functional.syntax._
+import play.api.libs.json.Format
+
+case class AmountInPence(value: Long) {
+  val inPounds: BigDecimal = (BigDecimal(value) / 100).setScale(2)
+  val formattedInPounds: String = getCurrencyInstance(UK).format(inPounds)
+}
+
+object AmountInPence {
+  implicit val format: Format[AmountInPence] = implicitly[Format[Long]].inmap(AmountInPence(_), _.value)
+}
