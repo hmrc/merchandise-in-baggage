388cdc05c5c7a039e12d16fbfa87ca20fe271142
SachaMcCormack <175815565+SachaMcCormack@users.noreply.github.com>
2025-04-02 13:08:39 +0100

    DDCE-6483: Removed Province of China from Taiwan (#151)
    * Removed Province of China from Taiwan
    
    * fix platops notifications

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,13 +2,13 @@ import sbt.*
 
 object AppDependencies {
 
-  private lazy val bootstrapPlayVersion = "9.5.0"
-  private lazy val hmrcMongoVersion     = "2.3.0"
+  private lazy val bootstrapPlayVersion = "9.11.0"
+  private lazy val hmrcMongoVersion     = "2.6.0"
 
   private val compile: Seq[ModuleID] = Seq(
     "uk.gov.hmrc"       %% "bootstrap-backend-play-30" % bootstrapPlayVersion,
     "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-30"        % hmrcMongoVersion,
-    "org.typelevel"     %% "cats-core"                 % "2.12.0",
+    "org.typelevel"     %% "cats-core"                 % "2.13.0",
     "com.beachape"      %% "enumeratum-play"           % "1.8.2"
   )
 
