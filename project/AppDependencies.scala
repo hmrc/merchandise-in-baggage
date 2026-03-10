54e6a82c83b3d57d1212b4ee453008f23dfdc502
lmugnai <11977337+lmugnai@users.noreply.github.com>
2026-03-10 17:46:18 +0000

    Update hmrc mongo version

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -3,7 +3,7 @@ import sbt.*
 object AppDependencies {
 
   private lazy val bootstrapPlayVersion = "10.7.0"
-  private lazy val hmrcMongoVersion     = "2.7.0"
+  private lazy val hmrcMongoVersion     = "2.12.0"
 
   private val compile: Seq[ModuleID] = Seq(
     "uk.gov.hmrc"       %% "bootstrap-backend-play-30" % bootstrapPlayVersion,
